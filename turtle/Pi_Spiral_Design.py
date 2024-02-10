0ee333d943b35ad540b2c9bfbe20c1d4c4df60b4
JP0030 <thekeen003@gmail.com>
2024-02-10 21:11:51 +0530

    in turtle folder new .py file added

---
+++ turtle/Pi_Spiral_Design.py
@@ -0,0 +1,26 @@
+
+import turtle
+colors = [ 'darkorange' , 'darkorange' , 'white' , 'blue' , 'green' , 'green' ]
+t= turtle.Pen()
+t.speed(10)
+turtle.bgcolor("black")
+
+for x in range(300):
+	
+	t.pencolor(colors[x%6]) 
+	t.width(x/100 + 1) 
+	t.forward(x) 
+	t.left(59) 
+	
+turtle.done()
+t.speed(20)
+turtle.bgcolor("black") 
+
+for x in range(300):
+	
+	t.pencolor(colors[x%6]) 
+	t.width(x/100 + 1) 
+	t.forward(x)
+	t.left(59) 
+
+turtle.done()
