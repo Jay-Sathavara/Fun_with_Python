4d5cc58b11a207462f589722e4ebd9ac12eac44f
JP0030 <thekeen003@gmail.com>
2024-02-10 21:26:50 +0530

    in turtle folder new .py file added

---
+++ turtle/Starry_Sky.py
@@ -0,0 +1,50 @@
+import turtle
+import random
+
+t = turtle.Turtle()
+
+w = turtle.Screen()
+
+t.speed(15)
+
+w.bgcolor("black")
+
+t.color("white")
+
+w.title("Starry Sky")
+
+def stars():
+	for i in range(6):
+		t.fd(10)
+		t.right(144)
+
+for i in range(120):
+
+	x = random.randint(-640, 640)
+	y = random.randint(-330, 330)
+	
+	stars()
+	
+	t.up()
+	
+	t.goto(x, y)
+	
+	t.down()
+
+t.up()
+
+t.goto(0, 180)
+
+t.down()
+
+t.color("white")
+
+t.begin_fill()
+
+t.circle(70)
+
+t.end_fill()
+
+t.hideturtle()
+
+w.exitonclick()
