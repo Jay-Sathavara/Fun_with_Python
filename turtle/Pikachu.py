ea6e2daaceea68a7ec34c7488cf20b1478188927
JP0030 <thekeen003@gmail.com>
2024-02-12 22:42:45 +0530

    ' Pikachu.py ' is about to complite in just in time ...

--- turtle/Pikachu.py
+++ turtle/Pikachu.py
@@ -87,3 +87,98 @@ class Shinchan:
             t.fd(a)
             l2.append(t.position())
         
+        t.seth(10)
+        t.circle(50, 15)
+        t.left(180)
+        t.circle(-50, 15)
+        t.circle(-50, 40)
+        t.seth(233)
+        t.circle(-50, 55)
+        t.left(180)
+        t.circle(50, 12.1)
+        t.end_fill()
+        #
+        self.meme(17, 54)
+        t.fillcolor('#DD716F')
+        t.begin_fill()
+        t.seth(145)
+        t.circle(40, 86)
+        t.penup()
+        for pos in reversed(l1[:20]):
+            t.goto(pos[0], pos[1] + 1.5)
+        for pos in l2[:20]:
+            t.goto(pos[0], pos[1] + 1.5)
+        t.pendown()
+        t.end_fill()
+        #
+        self.meme(-17, 94)
+        t.seth(8)
+        t.fd(4)
+        t.back(8)
+    #
+    def gaala1(self, x, y):
+        turtle.tracer(False)
+        t = self.t
+        self.meme(x, y)
+        t.seth(300)
+        t.fillcolor('#DD4D28')
+        t.begin_fill()
+        a = 2.3
+        for i in range(120):
+            if 0 <= i < 30 or 60 <= i < 90:
+                a -= 0.05
+                t.lt(3)
+                t.fd(a)
+            else:
+                a += 0.05
+                t.lt(3)
+                t.fd(a)
+        t.end_fill()
+        turtle.tracer(True)
+    def gaala2(self, x, y):
+        t = self.t
+        turtle.tracer(False)
+        self.meme(x, y)
+        t.seth(60)
+        t.fillcolor('#DD4D28')
+        t.begin_fill()
+        a = 2.3
+        for i in range(120):
+            if 0 <= i < 30 or 60 <= i < 90:
+                a -= 0.05
+                t.lt(3)
+                t.fd(a)
+            else:
+                a += 0.05
+                t.lt(3)
+                t.fd(a)
+        t.end_fill()
+        turtle.tracer(True)
+    def kaan1(self, x, y):
+        t = self.t
+        self.meme(x, y)
+        t.fillcolor('#000000')
+        t.begin_fill()
+        t.seth(330)
+        t.circle(100, 35)
+        t.seth(219)
+        t.circle(-300, 19)
+        t.seth(110)
+        t.circle(-30, 50)
+        t.circle(-300, 10)
+        t.end_fill()
+    def kaan2(self, x, y):
+        t = self.t
+        self.meme(x, y)
+        t.fillcolor('#000000')
+        t.begin_fill()
+        t.seth(300)
+        t.circle(-100, 30)
+        t.seth(35)
+        t.circle(300, 15)
+        t.circle(30, 50)
+        t.seth(190)
+        t.circle(300, 17)
+        t.end_fill()
+
+        
