9ab131e05817104550c74b845b42458a1aedbd91
JP0030 <thekeen003@gmail.com>
2024-02-14 23:43:50 +0530

    Doraemon is on the way

--- turtle/doraemon.py
+++ turtle/doraemon.py
@@ -5,6 +5,7 @@ def ankle(x,y):
     penup()
     goto(x,y)
     pendown()
+
 def eyes():
     fillcolor("#ffffff")
     begin_fill()
@@ -21,3 +22,30 @@ def eyes():
             fd(a)
     tracer(True)
     end_fill()
+
+def daari():
+    ankle(-32, 135)
+    seth(165)
+    fd(60)
+
+    ankle(-32, 125)
+    seth(180)
+    fd(60)
+
+    ankle(-32, 115)
+    seth(193)
+    fd(60)
+
+    ankle(37, 135)
+    seth(15)
+    fd(60)
+
+    ankle(37, 125)
+    seth(0)
+    fd(60)
+
+    ankle(37, 115)
+    seth(-13)
+    fd(60)
+
+
