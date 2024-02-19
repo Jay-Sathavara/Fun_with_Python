f819a7341f74e58625251f8b93efab8b0e3173c8
JP0030 <thekeen003@gmail.com>
2024-02-19 23:43:21 +0530

    Weather app

---
+++ python/Weather.py
@@ -0,0 +1,19 @@
+﻿# JP0030
+
+import time
+from tkinter import *
+from tkinter import messagebox as mb
+import requests
+from plyer import notification
+
+def getNotification():
+    cityName=place.get() 
+    baseUrl = "http://api.openweathermap.org/data/2.5/weather?" 
+    try:
+
+        url = baseUrl + "appid=" + 'd850f7f52bf19300a9eb4b0aa6b80f0d' + "&q=" + cityName  
+        response = requests.get(complete_url) 
+        x = response.json() 
+        y = x["main"] 
+ 
+     
