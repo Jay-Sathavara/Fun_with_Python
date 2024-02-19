6ef6d4ac9b6fbd3408278a73f980437ed2164450
JP0030 <thekeen003@gmail.com>
2024-02-19 23:36:50 +0530

    weight converter

--- python/weight converter.py
+++ python/weight converter.py
@@ -3,12 +3,17 @@
 import tkinter as tk
 from tkinter import *
 
-window=Tk()#creating a window
-window.title("PythonGeeks")#title of the window
-window.geometry("500x500")#geometry of the window
-#create a label
+window=Tk()
+window.title("PythonGeeks")
+window.geometry("500x500")
+
 Label(window,text="WEIGHT CONVERTER",font=("Arial", 20 ),bg="black",fg='yellow').pack()
 
-kg=tk.IntVar()#kg is integer type
+kg=tk.IntVar()
 
 def convert_to_gram():
+    kg1=kg.get() 
+    gram = float(kg1)*1000 
+    Label(window,text="This weight in grams would be",font=("Arial", 12 )).pack()
+    Label(window,text=gram,bg="red").pack()
+
