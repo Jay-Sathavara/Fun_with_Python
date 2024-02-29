97c7951da7c398dcd511f93cb4ca3530442c933e
JP0030 <thekeen003@gmail.com>
2024-02-29 21:34:59 +0530

    some changes made

--- Game/Hangman_JP0030.py
+++ Game/Hangman_JP0030.py
@@ -3,7 +3,7 @@
 import random
 import time
 
-print("\nWelcome to Hangman game by DataFlair\n")
+print("\nWelcome to Hangman game by JP0030\n")
 name = input("Enter your name: ")
 print("Hello " + name + "! Best of Luck!")
 time.sleep(2)
