b5788a49348743143054d77ea8d78b2bc662eae6
JP0030 <thekeen003@gmail.com>
2024-02-29 20:56:33 +0530

    new text game is onthe way

---
+++ Game/Hangman_DataFlair.py
@@ -0,0 +1,30 @@
+# JP0030
+
+import random
+import time
+
+# Initial Steps to invite in the game:
+print("\nWelcome to Hangman game by DataFlair\n")
+name = input("Enter your name: ")
+print("Hello " + name + "! Best of Luck!")
+time.sleep(2)
+print("The game is about to start!\n Let's play Hangman!")
+time.sleep(3)
+
+
+# The parameters we require to execute the game:
+def main():
+    global count
+    global display
+    global word
+    global already_guessed
+    global length
+    global play_game
+    words_to_guess = ["january","border","image","film","promise","kids","lungs","doll","rhyme","damage"
+                   ,"plants"]
+    word = random.choice(words_to_guess)
+    length = len(word)
+    count = 0
+    display = '_' * length
+    already_guessed = []
+    play_game = ""
