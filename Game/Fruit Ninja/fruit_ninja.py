08ad023af5eb91d9e4a42932a5fbf9ec98e9df16
JP0030 <thekeen003@gmail.com>
2024-03-01 21:53:26 +0530

    comic.ttf file added as text file

--- Game/Fruit Ninja/fruit_ninja.py
+++ Game/Fruit Ninja/fruit_ninja.py
@@ -1,1 +1,25 @@
 # JP0030
+
+import pygame, sys
+import os
+import random
+
+player_lives = 3                                                #keep track of lives
+score = 0                                                       #keeps track of score
+fruits = ['melon', 'orange', 'pomegranate', 'guava', 'bomb']    #entities in the game
+
+# initialize pygame and create window
+WIDTH = 800
+HEIGHT = 500
+FPS = 12                                                 #controls how often the gameDisplay should refresh. In our case, it will refresh every 1/12th second
+pygame.init()
+pygame.display.set_caption('Fruit-Ninja Game -- DataFlair')
+gameDisplay = pygame.display.set_mode((WIDTH, HEIGHT))   #setting game display size
+clock = pygame.time.Clock()
+
+# Define colors
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
