fd73665bdd785bdb3376d86b0eca4d4076cac299
JP0030 <thekeen003@gmail.com>
2024-03-01 21:55:57 +0530

    code is added

--- Game/Fruit Ninja/fruit_ninja.py
+++ Game/Fruit Ninja/fruit_ninja.py
@@ -4,22 +4,52 @@ import pygame, sys
 import os
 import random
 
-player_lives = 3                                                #keep track of lives
-score = 0                                                       #keeps track of score
-fruits = ['melon', 'orange', 'pomegranate', 'guava', 'bomb']    #entities in the game
+player_lives = 3
+score = 0     
+fruits = ['melon', 'orange', 'pomegranate', 'guava', 'bomb']    
 
-# initialize pygame and create window
+
 WIDTH = 800
 HEIGHT = 500
-FPS = 12                                                 #controls how often the gameDisplay should refresh. In our case, it will refresh every 1/12th second
+FPS = 12   
 pygame.init()
 pygame.display.set_caption('Fruit-Ninja Game -- DataFlair')
-gameDisplay = pygame.display.set_mode((WIDTH, HEIGHT))   #setting game display size
+gameDisplay = pygame.display.set_mode((WIDTH, HEIGHT))   
 clock = pygame.time.Clock()
 
-# Define colors
+
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 RED = (255,0,0)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
+
+background = pygame.image.load('back.jpg') 
+font = pygame.font.Font(os.path.join(os.getcwd(), 'comic.ttf'), 42)
+score_text = font.render('Score : ' + str(score), True, (255, 255, 255)) 
+lives_icon = pygame.image.load('images/white_lives.png')           
+
+def generate_random_fruits(fruit):
+    fruit_path = "images/" + fruit + ".png"
+    data[fruit] = {
+        'img': pygame.image.load(fruit_path),
+        'x' : random.randint(100,500),          
+        'y' : 800,
+        'speed_x': random.randint(-10,10),      
+        'speed_y': random.randint(-80, -60),  
+        'throw': False,                        
+        't': 0,                                
+        'hit': False,
+    }
+
+    if random.random() >= 0.75:    
+        data[fruit]['throw'] = True
+    else:
+        data[fruit]['throw'] = False
+
+data = {}
+for fruit in fruits:
+    generate_random_fruits(fruit)
+
+def hide_cross_lives(x, y):
+    gameDisplay.blit(pygame.image.load("images/red_lives.png"), (x, y))
