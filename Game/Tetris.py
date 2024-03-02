0776b443370704caa249a19ebc5e4c76a2fcccff
JP0030 <thekeen003@gmail.com>
2024-03-02 21:29:54 +0530

    New game is on the way

--- Game/Tetris.py
+++ Game/Tetris.py
@@ -24,3 +24,19 @@ blockSize = 20
 topLeft_x = (width - gameWidth) // 2
 topLeft_y = height - gameHeight - 50
 
+class Block:
+    x = 0
+    y = 0
+    n = 0
+    def __init__(self, x, y,n):
+        self.x = x
+        self.y = y
+        self.type = n
+        self.color = n
+        self.rotation = 0
+    def image(self):
+        return shapes[self.type][self.rotation]
+
+    def rotate(self):
+        self.rotation = (self.rotation + 1) % len(shapes[self.type])
+
