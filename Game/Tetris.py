8ebcda661e6c719022eec67c3b9526c050fe8ee9
JP0030 <thekeen003@gmail.com>
2024-03-02 21:37:34 +0530

    Tetris

--- Game/Tetris.py
+++ Game/Tetris.py
@@ -155,20 +155,17 @@ def startGame():
     pressing_down = False
     
     while not done:
-        #Create a new block if there is no moving block
         if game.block is None:
             game.new_block()
         if game.nextBlock is None:
             game.next_block()
-        counter += 1 #Keeping track if the time 
+        counter += 1 
         if counter > 100000:
             counter = 0
 
-        #Moving the block continuously with time or when down key is pressed
         if counter % (fps // game.level // 2) == 0 or pressing_down:
             if game.state == "start":
                 game.moveDown()
-        #Checking which key is pressed and running corresponding function
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
@@ -188,7 +185,6 @@ def startGame():
 
         screen.fill('#FFFFFF')
 
-        #Updating the game board regularly
         for i in range(game.height):
             for j in range(game.width):
                 pygame.draw.rect(screen, '#B2BEB5', [game.x + game.zoom * j, game.y + game.zoom * i, game.zoom, game.zoom], 1)
@@ -196,7 +192,6 @@ def startGame():
                     pygame.draw.rect(screen, shapeColors[game.field[i][j]],
                                      [game.x + game.zoom * j + 1, game.y + game.zoom * i + 1, game.zoom - 2, game.zoom - 1])
 
-        #Updating the board with the moving block
         if game.block is not None:
             for i in range(4):
                 for j in range(4):
@@ -207,14 +202,12 @@ def startGame():
                                           game.y + game.zoom * (i + game.block.y) + 1,
                                           game.zoom - 2, game.zoom - 2])
 
-        #Showing the score
         font = pygame.font.SysFont('Calibri', 40, True, False)
         font1 = pygame.font.SysFont('Calibri', 25, True, False)
         text = font.render("Score: " + str(game.score), True, '#000000')
         text_game_over = font.render("Game Over", True, '#000000')
         text_game_over1 = font.render("Press ESC", True, '#000000')
 
-        #Ending the game if state is gameover
         screen.blit(text, [300, 0])
         if game.state == "gameover":
             screen.blit(text_game_over, [300, 200])
@@ -227,7 +220,7 @@ def startGame():
 
 
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Tetris by DataFlair")
+pygame.display.set_caption("Tetris by JP0030")
 run = True
 while run:
     screen.fill((16, 57, 34 ))
