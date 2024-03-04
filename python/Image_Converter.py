7d1179fc856852cbdcd52f39819de59f1ebb0463
JP0030 <thekeen003@gmail.com>
2024-03-04 21:15:16 +0530

    Image Converter

--- python/Image_Converter.py
+++ python/Image_Converter.py
@@ -8,7 +8,7 @@ from pdf2image import convert_from_path
 
 root = Tk()
 root.geometry('600x250')
-root.title('DataFlair')
+root.title('JP0030')
 Label(root,text='Image Format Converter',font='arial 15').place(x=210,y=10)
 
 def browse():
@@ -18,3 +18,18 @@ def browse():
 
 Button(root,text='Browse an Image',command=browse,fg='blue',font='arial 10').place(x=250,y=45)
 
+
+def png_to_jpg():
+    global img
+    export_file_path = filedialog.asksaveasfilename(defaultextension='.jpg')
+    img.save(export_file_path)
+
+def jpg_to_png():
+    global img
+    export_file_path = filedialog.asksaveasfilename(defaultextension='.png')
+    img.save(export_file_path)
+
+Button(root,text='Png To Jpg',command=png_to_jpg,fg='red',font='arial 10').place(x=120,y=95)
+Button(root,text='Jpg To Png',command=jpg_to_png,fg='red',font='arial 10').place(x=450,y=95)
+
+root.mainloop()
