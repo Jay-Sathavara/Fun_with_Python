a4a004476b57769b3df0c86a0090af7a612609ab
JP0030 <thekeen003@gmail.com>
2024-03-10 21:10:05 +0530

    Linkedin Connections Project Code

--- python/Linkedin.py
+++ python/Linkedin.py
@@ -1,1 +1,10 @@
-Linkedin Connections Project Code
+from selenium import webdriver
+
+
+def login_to_linkedin():
+    Username=browser.find_element_by_name("session_key")
+    Username.send_keys("enter username")
+    password=browser.find_element_by_name("session_password")
+    password.send_keys("enter password")
+    browser.find_element_by_xpath("//button[@type='submit']").click()
+
