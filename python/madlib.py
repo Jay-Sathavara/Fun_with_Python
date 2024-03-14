244e117a817a3f69ec464c4d0113d5a20a8482c8
JP0030 <thekeen003@gmail.com>
2024-03-14 23:01:44 +0530

    Madlib

---
+++ python/madlib.py
@@ -0,0 +1,12 @@
+# JP0030
+
+from tkinter
+import *
+
+root = Tk()
+root.geometry('300x300')
+root.title('JP0030 - Mad Libs Generator')
+Label(root, text= 'Mad Libs Generator \n Have Fun!' , font = 'arial 20 bold').pack()
+Label(root, text = 'Click Any One :', font = 'arial 15 bold').place(x=40, y=80)
+
+
