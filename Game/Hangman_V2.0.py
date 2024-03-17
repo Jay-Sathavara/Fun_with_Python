9074400c969bd99f856c12a972ed8016ea363a6b
JP0030 <thekeen003@gmail.com>
2024-03-17 23:03:15 +0530

    Hangman V2.0

--- Game/Hangman_V2.0.py
+++ Game/Hangman_V2.0.py
@@ -17,3 +17,20 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Hangman")
 icon = pygame.image.load("img_9.png")
 pygame.display.set_icon(icon)
+
+game_over = False
+
+row = 2
+col = 13
+gap = 20
+size = 40
+boxes = []
+
+for row in range(row):
+    for col in range(col):
+        x = ((col * gap) + gap) + (size * col)
+        y = ((row * gap) + gap) + (size * row) + 330
+        box = pygame.Rect(x, y, size, size)
+        boxes.append(box)
+
+        
