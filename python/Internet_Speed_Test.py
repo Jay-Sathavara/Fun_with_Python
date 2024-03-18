4130b96ae97df1dc879b6b0218225380d8582af0
JP0030 <thekeen003@gmail.com>
2024-03-18 20:34:39 +0530

    Internet Speed Test

--- python/Internet_Speed_Test.py
+++ python/Internet_Speed_Test.py
@@ -3,7 +3,6 @@
 from tkinter import *
 from speedtest import Speedtest
 
-# Creation OF Function
 def update_text():
     speed_test = Speedtest()
     download = speed_test.download()
@@ -12,3 +11,15 @@ def update_text():
     upload_speed = round(upload / (10**6), 2)
     down_label.config(text= "Download Speed - " + str(download_speed) + "Mbps")
     up_label.config(text= "Upload Speed - " + str(upload_speed) + "Mbps")
+
+window = Tk()
+window.title("Internet Speed Testing")
+window.geometry('420x250+250+150')
+button = Button(window, text="Press Here to Check Speed", width=50, command=update_text,background = '#49A')
+button.pack()
+down_label = Label(window, text="")
+down_label.pack()
+up_label = Label(window, text="")
+up_label.pack()
+
+window.mainloop()
