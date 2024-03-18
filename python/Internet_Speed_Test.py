43fb3b10d91b27ad99ea24f4da20a8ca43c7c194
JP0030 <thekeen003@gmail.com>
2024-03-18 20:33:49 +0530

    Internet Speed Test

--- python/Internet_Speed_Test.py
+++ python/Internet_Speed_Test.py
@@ -1,3 +1,14 @@
 # JP0030
 
+from tkinter import *
+from speedtest import Speedtest
 
+# Creation OF Function
+def update_text():
+    speed_test = Speedtest()
+    download = speed_test.download()
+    upload = speed_test.upload()
+    download_speed = round(download / (10**6), 2)
+    upload_speed = round(upload / (10**6), 2)
+    down_label.config(text= "Download Speed - " + str(download_speed) + "Mbps")
+    up_label.config(text= "Upload Speed - " + str(upload_speed) + "Mbps")
