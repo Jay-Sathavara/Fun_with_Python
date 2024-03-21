0e916b282165736a6b79dc297a11867bdbfe82e6
JP0030 <thekeen003@gmail.com>
2024-03-21 21:02:31 +0530

    Image Into Sketch

--- python/Convert Image Into Sketch/Image_Into_Sketch.py
+++ python/Convert Image Into Sketch/Image_Into_Sketch.py
@@ -8,3 +8,10 @@ gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
 invert_image = cv.bitwise_not(gray_image)
 
+blur_image = cv.GaussianBlur(invert_image, (21,21), 0)
+
+invert_blur = cv.bitwise_not(blur_image)
+
+sketch = cv.divide(gray_image, invert_blur, scale=256.0)
+
+cv.imwrite("Sketch4.png", invert_blur)
