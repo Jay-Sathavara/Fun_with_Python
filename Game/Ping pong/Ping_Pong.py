df1ee2df354e0e147062c3f38881dcbe120d0583
JP0030 <thekeen003@gmail.com>
2024-03-23 19:39:02 +0530

    Ping Pong

--- Game/Ping pong/Ping_Pong.py
+++ Game/Ping pong/Ping_Pong.py
@@ -1,1 +1,22 @@
 # JP0030
+
+import pygame
+import sys
+import random
+import time
+
+pygame.init()
+
+c = pygame.time.Clock()
+
+width = 900
+height = 600
+
+screen = pygame.display.set_mode((width, height))
+
+pygame.display.set_caption("Ping Pong Game")
+
+ball = pygame.Rect(width / 2 - 15, height / 2 - 15, 30, 30, )
+player1 = pygame.Rect(width - 20, height / 2 - 70, 10, 140)
+player2 = pygame.Rect(10, height / 2 - 70, 10, 140)
+
