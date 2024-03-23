c5c626790ff6481b51d810915cac9f710e128bb1
JP0030 <thekeen003@gmail.com>
2024-03-23 19:39:52 +0530

    Ping Pong

--- Game/Ping pong/Ping_Pong.py
+++ Game/Ping pong/Ping_Pong.py
@@ -20,3 +20,35 @@ ball = pygame.Rect(width / 2 - 15, height / 2 - 15, 30, 30, )
 player1 = pygame.Rect(width - 20, height / 2 - 70, 10, 140)
 player2 = pygame.Rect(10, height / 2 - 70, 10, 140)
 
+ball_speedx = 6 * random.choice((1, -1))
+ball_speedy = 6 * random.choice((1, -1))
+player1_speed = 0
+player2_speed = 6
+player1_score = 0
+player2_score = 0
+
+
+def ball_movement():
+    global ball_speedx, ball_speedy, player1_score, player2_score
+    ball.x += ball_speedx
+    ball.y += ball_speedy
+
+    if ball.top <= 0 or ball.bottom >= height:
+        ball_speedy *= -1
+    if ball.left <= 0:
+        player1_score += 1
+        ball_restart()
+    if ball.right >= width:
+        player2_score += 1
+        ball_restart()
+
+    if ball.colliderect(player1) or ball.colliderect(player2):
+        ball_speedx *= -1
+
+def player1_movement():
+    global player1_speed
+    player1.y += player1_speed
+    if player1.top <= 0:
+        player1.top = 0
+    if player1.bottom >= height:
+        player1.bottom = height
