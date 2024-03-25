23e36768f0c638b665d0c5f3cfa01f2f23fb1cc1
JP0030 <thekeen003@gmail.com>
2024-03-25 21:16:36 +0530

    Photo Collage Maker

--- python/Photo_Collage_Maker.py
+++ python/Photo_Collage_Maker.py
@@ -1,2 +1,10 @@
 # JP0030
 
+from PIL import Image
+
+image1 = Image.open("image1.png")
+image1=image1.resize((500,500))
+image2 = Image.open("image2.png")
+image2=image2.resize((500,500))
+image3 = Image.open("image3.png")
+image3=image3.resize((500,500))
