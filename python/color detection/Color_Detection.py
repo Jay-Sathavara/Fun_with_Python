9f563fe09211a4b4b35c927afd0ed0e74888fba0
JP0030 <thekeen003@gmail.com>
2024-03-26 22:20:54 +0530

    color detection

--- python/color detection/Color_Detection.py
+++ python/color detection/Color_Detection.py
@@ -4,3 +4,21 @@
 # Kedaar_Nath.jpg – sample image for experimenting.
 # Colors.csv – a file that contains our dataset.
 
+import cv2
+import numpy as np
+import pandas as pd
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--image', required=True, help="Image Path")
+args = vars(ap.parse_args())
+img_path = args['image']
+
+img = cv2.imread(img_path)
+
+clicked = False
+r = g = b = xpos = ypos = 0
+
+index=["color","color_name","hex","R","G","B"]
+csv = pd.read_csv('colors.csv', names=index, header=None)
+
